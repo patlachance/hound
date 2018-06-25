22881459d2a7ef7f17fa217cc7fcc9339a0b5921
Greg Lazarev <greg.lazarev@gmail.com>
2018-06-25 11:56:15 -0700

    Support GitHub app flow

--- app/models/marketplace_plan.rb
+++ app/models/marketplace_plan.rb
@@ -1,43 +1,55 @@
-class MarketplacePlan
-  include ActiveModel::Serialization
+# frozen_string_literal: true
 
+class MarketplacePlan
+  MARKETPLACE_UPGRADE_URL = "https://www.github.com/marketplace/hound"
   PLANS = [
-    { id: 1061, price: 0, range: 0..0, title: "Open Source" },
-    { id: 1062, price: 49, range: 1..4, title: "Chihuahua" },
-    { id: 1063, price: 149, range: 5..20, title: "Octodog" },
+    OpenStruct.new(id: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYx", repos: 0),
+    OpenStruct.new(id: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYy", repos: 4),
+    OpenStruct.new(id: "MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW4xMDYz", repos: 20),
   ].freeze
 
-  attr_reader :id, :price, :title
-
-  def initialize(id:, range:, price:, title:)
-    @id = id
-    @range = range
-    @price = price
-    @title = title
+  def initialize(owner)
+    @owner = owner
   end
 
-  def self.all
-    PLANS.map { |plan| new(plan) }
+  def upgrade?
+    owner.active_private_repos_count >= current_plan.repos
   end
 
-  def self.find_by(count:)
-    found = PLANS.detect { |plan| plan.fetch(:range).include?(count) }
-    new(found)
+  def current_plan
+    @_current_plan ||= PLANS.
+      detect { |plan| marketplace_plan.id == plan.id } || PLANS.first
   end
 
-  def ==(other)
-    id == other.id
+  def next_plan
+    @_next_plan ||= PLANS[
+      PLANS.find_index { |plan| plan.id == current_plan.id } + 1
+    ]
   end
 
-  def allowance
-    range.max
+  def previous_plan
+    @_previous_plan ||= current_plan.repos.positive? &&
+      PLANS[PLANS.find_index { |plan| plan.id == current_plan.id } - 1]
   end
 
-  def open_source?
-    price.zero?
+  def upgrade_url
+    "#{MARKETPLACE_UPGRADE_URL}/order/#{next_plan.id}?account=#{owner.name}"
+  end
+
+  def downgrade_url
+    "#{MARKETPLACE_UPGRADE_URL}/order/#{previous_plan.id}?account=#{owner.name}"
   end
 
   private
 
-  attr_reader :range
+  attr_reader :owner
+
+  def marketplace_plan
+    @_marketplace_plan ||= app.
+      plan_for_account(owner.github_id) || OpenStruct.new(id: nil)
+  end
+
+  def app
+    @_app ||= GitHubApi.new(AppToken.new.generate)
+  end
 end
